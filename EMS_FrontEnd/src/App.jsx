79428a70eed6037e0ac28116263e6ff73628eafe
Makena-Wambui <iammakena@gmail.com>
2024-12-16 23:26:11 +0300

    feat: Employee Dashboard
    - Defined `EMS_FrontEnd/src/pages/EmployeeDashboard.jsx`
    - Fixed 'authContext' to 'AuthContext' in 'EMS_FrontEnd/src/main.jsx'
    - Fixed destructuring error in 'EMS_FrontEnd/src/pages/AdminDashboard.jsx'

--- EMS_FrontEnd/src/App.jsx
+++ EMS_FrontEnd/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import AdminDashboard from "./pages/AdminDashboard";
+import EmployeeDashboard from "./pages/EmployeeDashboard";
 
 {
   /*
@@ -20,6 +21,11 @@ function App() {
           {/* Render the Login component */}
           <Route path="/admin-dashboard" element={<AdminDashboard />} />{" "}
           {/* Render the AdminDashboard component */}
+          <Route
+            path="/employee-dashboard"
+            element={<EmployeeDashboard />}
+          />{" "}
+          {/* Render the EmployeeDashboard component */}
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ EMS_FrontEnd/src/pages/EmployeeDashboard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const EmployeeDashboard = () => {
+  // The EmployeeDashboard component is responsible for rendering the employee dashboard.
+  return <div>EmployeeDashboard</div>;
+};
+
+export default EmployeeDashboard;
