ff42563ca1ee8f4b6b4bdc7c23a03cea0ddd99c3
Makena-Wambui <iammakena@gmail.com>
2024-12-28 12:04:44 +0300

    feat: Employee dashboard - Implemented logout functionality

--- EMS_FrontEnd/src/components/dashboard/Navbar.jsx
+++ EMS_FrontEnd/src/components/dashboard/Navbar.jsx
@@ -1,11 +1,17 @@
 import { useAuth } from "../../context/authContext";
 
+{
+  /* Navbar component */
+}
 const Navbar = () => {
-  const { user } = useAuth();
+  const { user, logout } = useAuth(); // Get user and logout function from auth context
   return (
     <div className="bg-teal-600 text-white h-12 flex items-center justify-between px-5">
       <p>Welcome {user.name}</p>
-      <button className="px-4 py-1 bg-teal-700 hover:bg-teal-900">
+      <button
+        className="px-4 py-1 bg-teal-700 hover:bg-teal-900"
+        onClick={logout} // Call logout function on click
+      >
         Logout
       </button>
     </div>
